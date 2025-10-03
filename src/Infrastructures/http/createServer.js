4bff65b2ca3b503a11863d4e60251701628569ef
Febriand1 <fdirga63@gmail.com>
2025-10-04 04:52:50 +0700

    kembalikan

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -69,35 +69,35 @@ const createServer = async (container) => {
     },
   });
 
-  // server.ext('onPreResponse', (request, h) => {
-  //   const { response } = request;
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
 
-  //   if (response instanceof Error) {
-  //     const translatedError = DomainErrorTranslator.translate(response);
+    if (response instanceof Error) {
+      const translatedError = DomainErrorTranslator.translate(response);
 
-  //     if (translatedError instanceof ClientError) {
-  //       const newResponse = h.response({
-  //         status: 'fail',
-  //         message: translatedError.message,
-  //       });
-  //       newResponse.code(translatedError.statusCode);
-  //       return newResponse;
-  //     }
+      if (translatedError instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: translatedError.message,
+        });
+        newResponse.code(translatedError.statusCode);
+        return newResponse;
+      }
 
-  //     if (!translatedError.isServer) {
-  //       return h.continue;
-  //     }
+      if (!translatedError.isServer) {
+        return h.continue;
+      }
 
-  //     const newResponse = h.response({
-  //       status: 'error',
-  //       message: 'terjadi kegagalan pada server kami',
-  //     });
-  //     newResponse.code(500);
-  //     return newResponse;
-  //   }
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
 
-  //   return h.continue;
-  // });
+    return h.continue;
+  });
 
   return server;
 };
